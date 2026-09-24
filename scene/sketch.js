084b64878f76d2f170a4e2bb0d49f208462bf7c9
fiIipd <3903880@spsd.sk.ca>
2026-09-23 22:12:03 -0600

    added working eyes
    played with some functions to see how i can make pupils follow my cursor

--- scene/sketch.js
+++ scene/sketch.js
@@ -3,30 +3,48 @@
 // Sept 22, 2026
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - https://www.youtube.com/watch?v=XOk0aGwZYn8 <- used to understand atan2
 
+let leftEyeX, yForEyes, rightEyeX;
+let eyeSize = 50;
+let pupilSize = 20;
 
-let button = false;
-let x = 50;
-let y = 50;
-let w = 100;
-let h = 75;
 async function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
+  leftEyeX = width/2 - 50;
+  rightEyeX = width/2 + 50 ;
+  yForEyes = height/2;
+
 }
 
-function draw() {
+async function draw() {
   background(220);
-  if (button){
-    background("black");
-  }
-  rect(x,y,w,h);
+  drawEye(leftEyeX,yForEyes);
+  drawEye(rightEyeX, yForEyes);
 
 }
-function mousePressed() {
-  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
-    button = !button;
-    circle(mouseX,mouseY,30);
-  }
+async function drawEye(eyeX, eyeY){
+  //Saves coordinates before changing
+  push();
+
+  //Make white part of eye
+  fill("white");
+  circle(eyeX, eyeY, eyeSize);
+
+  let xDistance = mouseX - eyeX; //Horizontal distance from the eye center to the mouse X position
+  let yDistance = mouseY - eyeY; //Same thing but vertical and mouse Y
+
+  //Make point of rotation the middle of the eye
+  translate(eyeX, eyeY);
+
+  let angle = atan2 (yDistance, xDistance) //tells you which direction the mouse is from the eye center
+                                    
+
+  //Rotate the pupil around the middle of the eye
+  rotate(angle);
+  fill("black");
+  circle(pupilSize - 5, 0, pupilSize);
+
+  //Restores old coordinates
+  pop();
 }
