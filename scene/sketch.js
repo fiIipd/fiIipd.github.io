ea43ed9a9a2a9f6e381ffec1adda287951431437
fiIipd <3903880@spsd.sk.ca>
2026-09-24 13:04:41 -0600

    added head

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,25 +5,27 @@
 // Extra for Experts:
 // - https://www.youtube.com/watch?v=XOk0aGwZYn8 <- used to understand atan2
 
-let leftEyeX, yForEyes, rightEyeX;
+let leftEyeX, rightEyeX, yForEyes;
 let eyeSize = 50;
 let pupilSize = 20;
+let eyeDistance = 50;
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
-  leftEyeX = width/2 - 50;
-  rightEyeX = width/2 + 50 ;
+  leftEyeX = width/2 - eyeDistance;
+  rightEyeX = width/2 + eyeDistance;
   yForEyes = height/2;
+  background(220);
+  drawFace();
 
 }
 
 async function draw() {
-  background(220);
   drawEye(leftEyeX,yForEyes);
   drawEye(rightEyeX, yForEyes);
 
 }
-async function drawEye(eyeX, eyeY){
+function drawEye(eyeX, eyeY){
   //Saves coordinates before changing
   push();
 
@@ -37,7 +39,7 @@ async function drawEye(eyeX, eyeY){
   //Make point of rotation the middle of the eye
   translate(eyeX, eyeY);
 
-  let angle = atan2 (yDistance, xDistance) //tells you which direction the mouse is from the eye center
+  let angle = atan2 (yDistance, xDistance); //tells you which direction the mouse is from the eye center
                                     
 
   //Rotate the pupil around the middle of the eye
@@ -47,4 +49,9 @@ async function drawEye(eyeX, eyeY){
 
   //Restores old coordinates
   pop();
+}
+function drawFace(){
+  fill(251, 230, 199);
+  circle(width/2, height/2, eyeSize * 5);
+  arc(width/2, height/2 - 30, 150, 30);
 }
